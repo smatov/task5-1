51ff5d8e98e40eb621b14fdec0efa462927d2fb9
dev <dev@debian-vbx.(none)>
2013-03-29 13:23:28 +0400

    fixed bugs

--- main.c
+++ main.c
@@ -16,59 +16,139 @@
 #include <fcntl.h>
 
 /*char getch() {
-	char c;
-	scanf("%c",&c);
-	return c;
-}*/
-
+ char c;
+ scanf("%c",&c);
+ return c;
+ }*/
 
 float getfloat() {
-	float ans=0;
-	int *fp=malloc(sizeof(char));
-	int *sp=malloc(sizeof(char));
+	float ans = 0;
+	int *fp = malloc(sizeof(char));
+	int *sp = malloc(sizeof(char));
+	int *tp = malloc(sizeof(char));
 	char c;
+	int f = 0;
 	int sg = 1;
-	int cnt=1;
+	int cnt = 1;
 	printf("Enter float value\n");
 	/*integer part*/
-	while ((c=getchar()) && c != '.') {
-		if (c == '-') {
-			sg = -1;
-			continue;
-		}
-		if (c >= '0' && c <= '9') {
-			*fp = *fp * 10 + (c - '0');
-		} else {
-			printf("Incorrect data entered\n");
-			exit(1);
-		}
 
-	}
-	/*other part*/
-	if (c == '.')
-	{
-		while ((c=getchar()) && c != ' '&& c != '\n' && c != EOF) {
-			if (c >= '0' && c <= '9') {
-						*sp = *sp * 10 + (c - '0');
-						cnt*=10;
-		} else {
-			printf("Incorrect data entered\n");
-			exit(1);
+	c = getchar();
+	if (c == '-' || c == '+') {
+		sg = c == '-' ? -1 : 1;
+		c = getchar();
+		if (c >= '0' && c <= '9')
+			while (c >= '0' && c <= '9') {
+				*fp = *fp * 10 + (c - '0');
+				if (!(c = getchar()))
+					break;
+
+				if (c == '\n') {
+					ans = *fp;
+					return ans * sg;
+				}
+			}
+	} else if (c >= '0' && c <= '9')
+		while (c >= '0' && c <= '9') {
+			*fp = *fp * 10 + (c - '0');
+			if (!(c = getchar()))
+				break;
+
+			if (c == '\n') {
+				ans = *fp;
+				return ans * sg;
 			}
 		}
-	}else {
-		printf("The value is not float\n");
+	else {
+		printf("Incorrect data entered\n");
 		exit(1);
 	}
-	ans=*fp+((float)*sp)/cnt;
-	ans*=sg;
-	return ans;
 
+	if (c == '\n') {
+		ans = *fp;
+		return ans * sg;
+	}
+	if (c == '.') {
+		c = getchar();
+		if (c >= '0' && c <= '9')
+			while (c >= '0' && c <= '9') {
+				*sp = *sp * 10 + (c - '0');
+				cnt *= 10;
+				if (!(c = getchar()) || c == '\n')
+					break;
+
+			}
+
+	} else if (c == 'e') {
+		f = 1;
+		c = getchar();
+		if (c == '\n') {
+			printf("Incorrect data entered\n");
+			exit(1);
+		}
+		if (c == '-' || c == '+') {
+			sg = c == '-' ? -1 : 1;
+			c = getchar();
+			if (c >= '0' && c <= '9')
+				while (c >= '0' && c <= '9') {
+					*tp = *tp * 10 + (c - '0');
+					if (!(c = getchar()))
+						break;
+
+					if (c == '\n') {
+
+						int x = *tp;
+						int st = 1;
+						int i;
+						for (i = 0; i < x; i++)
+							st *= 10;
+						ans = *fp;
+						ans = (sg > 0) ? (ans * st) : (ans / st);
+						return ans;
+					} else {
+						printf("Incorrect data entered\n");
+						exit(1);
+					}
+				}
+		}
+
+		if (c >= '0' && c <= '9')
+			while (c >= '0' && c <= '9') {
+				*tp = *tp * 10 + (c - '0');
+				if (!(c = getchar()) || c == '\n')
+					break;
+
+			}
+		if (c != '\n') {
+			printf("Incorrect data entered\n");
+			exit(1);
+		}
+
+	} else {
+		printf("Incorrect data entered\n");
+		exit(1);
+	}
+
+	if (f == 0) {
+		ans = *fp + ((float) *sp) / cnt;
+		ans *= sg;
+		return ans;
+	} else {
+		int x = *tp;
+		int st = 1;
+		int i;
+		for (i = 0; i < x; i++)
+			st *= 10;
+		ans = *fp;
+		ans *= st;
+		return ans;
+
+	}
 }
 
 int main() {
-	float f=getfloat();
-	printf("%.4f\n",f);
+	float f = getfloat();
+	printf("%.9f\n", f);
 	return 0;
 
 }
